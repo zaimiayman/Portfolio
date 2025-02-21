422ab98f6a10c62bb8232273bb20287ea355a692
zaimi <zaimiayman8@gmail.com>
2025-02-21 11:09:44 +0100

    First

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Projet from "./components/Projet";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+   <div>
+      <Navbar />
+      <Hero />
+      <About/>
+      <Services/>
+      <Projet/>
+      <Skills/>
+      <Contact/>
+      <Footer />
+      </div>
+  );
+};
+
+export default App;
